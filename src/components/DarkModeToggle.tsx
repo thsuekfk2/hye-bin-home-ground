4f9ee13899260be0da172a166cec47fa061d7207
thsuekfk2 <thsuekfk2@naver.com>
2023-04-25 23:31:05 +0900

    feat : dark mode toggle button & theme

---
+++ src/components/DarkModeToggle.tsx
@@ -0,0 +1,38 @@
+import styled from "@emotion/styled";
+
+interface DarkModeToggleProps {
+  darkMode: boolean;
+}
+
+export const DarkModeToggle = ({ handleTheme, darkMode }: any) => {
+  return (
+    <Total darkMode={darkMode} onClick={() => handleTheme()}>
+      <div className="dark-light">
+        <svg viewBox="0 0 24 24">
+          <path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z" />
+        </svg>
+      </div>
+    </Total>
+  );
+};
+
+const Total = styled.div<DarkModeToggleProps>`
+  .dark-light {
+    position: fixed;
+    bottom: 30px;
+    right: 30px;
+    background-color: ${(props) => (props.darkMode ? "black" : "white")};
+    box-shadow: -1px 3px 8px -1px rgba(0, 0, 0, 0.2);
+    padding: 8px;
+    border-radius: 50%;
+    z-index: 3;
+    cursor: pointer;
+    svg {
+      width: 24px;
+      flex-shrink: 0;
+      fill: #ffce45;
+      stroke: ${(props) => (props.darkMode ? "#ffce45" : "black")};
+      transition: 0.5s;
+    }
+  }
+`;
---
+++ styles/theme.ts
@@ -0,0 +1,23 @@
+export const lightTheme = {
+  text: "#000",
+  background: "#fff",
+  buttonText: "#000",
+  buttonTextHover: "#fff",
+  buttonBorder: "#000",
+  buttonBg: "rgba(0, 0, 0, 0)",
+  buttonBgHover: "rgba(0, 0, 0, 1)",
+  opacityBox: "rgba(0, 0, 0, 0.5)",
+  invertColor: "#fff",
+};
+
+export const darkTheme = {
+  text: "#fff",
+  background: "#121212",
+  buttonText: "#fff",
+  buttonTextHover: "#000",
+  buttonBorder: "#fff",
+  buttonBg: "rgba(255, 255, 255, 0)",
+  buttonBgHover: "rgba(255, 255, 255, 1)",
+  opacityBox: "rgba(255, 255, 255, 0.5)",
+  invertColor: "#000",
+};
