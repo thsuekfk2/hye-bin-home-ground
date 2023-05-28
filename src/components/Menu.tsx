90f7dfe9b3ead6ea2c743dd4f158f22a48bb508e
thsuekfk2 <thsuekfk2@naver.com>
2023-05-28 19:27:29 +0900

    feat : add menu component

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,18 +1,82 @@
+import { keyframes, useTheme } from "@emotion/react";
 import styled from "@emotion/styled";
+import { useState } from "react";
+import { AiFillGithub, AiFillHeart, AiFillHome } from "react-icons/ai";
+import { FaBloggerB } from "react-icons/fa";
+import { useAtom } from "jotai";
+import { cursorAtom } from "../states/cursorAtom";
+
+interface MenuIcon {
+  isOpenMenu: boolean;
+}
 
 export const Menu = () => {
+  const [isOpenMenu, setOpenMenu] = useState(false);
+  const [, setCursorColor] = useAtom(cursorAtom);
+  const theme = useTheme();
+
   return (
-    <MenuBox onClick={() => {}}>
-      <div className="hamburger-menu">
-        <div className="bar-top"></div>
-        <div className="bar-middle"></div>
-        <div className="bar-bottom"></div>
-      </div>
-    </MenuBox>
+    <>
+      <MenuIcon
+        onMouseEnter={() => {
+          setCursorColor((prev) => ({
+            ...prev,
+            color: theme.convertColor,
+            size: 50,
+          }));
+        }}
+        onMouseLeave={() => {
+          setCursorColor((prev) => ({
+            ...prev,
+            color: "black",
+            size: 16,
+          }));
+        }}
+        isOpenMenu={isOpenMenu}
+        onClick={() => {
+          setOpenMenu((prev) => !prev);
+        }}
+      >
+        <div className="hamburger-menu">
+          <div className="bar-top"></div>
+          <div className="bar-middle"></div>
+          <div className="bar-bottom"></div>
+        </div>
+      </MenuIcon>
+      {isOpenMenu && (
+        <MenuContent>
+          <div className="menu-item">
+            <AiFillHome />
+          </div>
+          <div className="menu-item">
+            <AiFillHeart />
+          </div>
+          <div
+            className="menu-item"
+            onClick={() =>
+              window.open("https://github.com/thsuekfk2", "_blank")
+            }
+          >
+            <AiFillGithub />
+          </div>
+          <div
+            className="menu-item"
+            onClick={() =>
+              window.open(
+                "https://hyebin-development-blog.tistory.com/",
+                "_blank"
+              )
+            }
+          >
+            <FaBloggerB />
+          </div>
+        </MenuContent>
+      )}
+    </>
   );
 };
 
-const MenuBox = styled.div`
+const MenuIcon = styled.div<MenuIcon>`
   position: fixed;
   z-index: 2;
   right: 0;
@@ -36,14 +100,59 @@ const MenuBox = styled.div`
     transform-origin: left;
     transition: all 0.5s;
   }
-  .hamburger-menu:hover .bar-top {
-    transform: rotate(45deg);
+
+  .bar-top {
+    transform: ${(props) => props.isOpenMenu && "rotate(45deg)"};
   }
-  .hamburger-menu:hover .bar-middle {
-    transform: translateX(10px);
+  .bar-middle {
+    transform: ${(props) => props.isOpenMenu && "translateX(10px)"};
+    opacity: ${(props) => props.isOpenMenu && 0};
+  }
+  .bar-bottom {
+    transform: ${(props) => props.isOpenMenu && "rotate(-45deg)"};
+  }
+`;
+
+const textFade = keyframes`
+  0% {
     opacity: 0;
   }
-  .hamburger-menu:hover .bar-bottom {
-    transform: rotate(-45deg);
+  50% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 0.7;
+  }
+`;
+
+const MenuContent = styled.div`
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  z-index: 1;
+  right: 20px;
+  top: 80px;
+  width: 60px;
+  height: 400px;
+  background-color: ${(props) => props.theme.background};
+  color: ${(props) => props.theme.convertColor};
+  border-radius: 10px;
+  opacity: 0.7;
+  animation: ${textFade} 0.5s linear alternate;
+  .menu-item {
+    cursor: pointer;
+    width: 90%;
+    font-size: 30px;
+    font-weight: 800;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 20%;
+    border-radius: 3px;
+    &:hover {
+      background-color: #4dc00028;
+    }
   }
 `;
